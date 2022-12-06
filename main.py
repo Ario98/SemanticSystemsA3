37bd7fdbef178d275a93071fba8b573141e297af
Ario98 <bliteum@protonmail.com>
2022-12-06 17:39:08 +0100

    Implemented remaining functionalities. TODO: Fix instance and property functions. Add more descriptions

--- main.py
+++ main.py
@@ -32,13 +32,35 @@ def addclass(name, subclass):
     print('Class {} added.'.format(name))
 
 @my_app.command()
-def showqueries():
-    rdf.show_queries()
-    print("Showing query.")
+@click.argument('domain',  type=str)
+@click.argument('property', type=str)
+@click.argument('range', type=str)
+def addproperty(domain, property, range):
+    rdf.add_property(domain, property, range)
+    print('Property {} added.'.format(property))
 
 @my_app.command()
-def executequeries():
-    rdf.execute_query()
+@click.argument('objectname', type=str)
+@click.argument('instancename', type=str)
+def addinstance(objectname, instancename):
+    # Fix
+    rdf.add_instance(objectname, instancename)
+    print('Instance {} added.'.format(instancename))
+
+@my_app.command()
+def showqueries():
+    print('Showing available queries:')
+    rdf.show_queries()
+    print("Call the function executequery with your chosen query to execute it. Example: executequery 1.")
+
+@my_app.command()
+@click.argument('querychoice', type=int)
+def executequery(querychoice):
+    rdf.execute_query(querychoice)
+
+@my_app.command()
+def executeconstructquery():
+    rdf.execute_construct_query()
 
 @my_app.command()
 def exportgraph():
--- rdf.py
+++ rdf.py
@@ -35,15 +35,38 @@ class rdf_module:
         for query in os.listdir('input/query'):
             print(query)
 
-    def execute_query(self):
-        # -- QUERIES
+    def execute_query(self, queryChoice):
+        self.queryChoice = queryChoice
+        query_list = []
+
         for query in os.listdir('input/query'):
             q = open('input/query/' + query).read()
-            result = self.g.query(q)
+            query_list.append(q)
+        
+        if self.queryChoice == 3:
+            print('This is a construct query. Call executeconstructquery to execute it.')
+            return
+        else:
+            result = self.g.query(query_list[self.queryChoice - 1])
             print("--- "+query+" ---")
             for row in result:
                 print(row)
 
+    def execute_construct_query(self):
+        query_list = []
+
+        for query in os.listdir('input/query'):
+            q = open('input/query/' + query).read()
+            query_list.append(q)
+
+        result = self.g.query(query_list[2])
+        print("--- "+query+" ---")
+        for row in result:
+            print(row)
+
+        print('Saving the result as a Turtle file to the export folder.')
+        result.serialize(format="turtle", destination= "export/constructResult.ttl")
+ 
     def export_graph(self, export_type):
         self.export_type = export_type
 
@@ -67,3 +90,28 @@ class rdf_module:
             self.g.add((self.URIname, RDFS.subClassOf, self.URIsubclass))
 
         self.g.add((self.URIname, RDFS.label, Literal(self.name)))
+
+    def add_property(self, domain, property, range):
+        # TODO: Fix
+        self.domain = domain
+        self.property = property
+        self.range = range
+
+        self.URIdomain = URIRef("http://semantics.id/ns/example/film#" + self.domain)
+        self.URIproperty = URIRef("http://semantics.id/ns/example/film#" + self.property)
+        self.URIrange = URIRef("http://semantics.id/ns/example/film#" + self.range)
+
+        self.g.add((self.URIproperty, RDF.type ,OWL.ObjectProperty))
+        self.g.add((self.URIproperty, RDFS.label, Literal(self.property)))
+        self.g.add((self.URIproperty, RDFS.domain, self.domain))
+        self.g.add((self.URIproperty, RDFS.range, self.range))
+
+    def add_instance(self, objectName, instanceName):
+        self.objectName = objectName
+        self.instanceName = instanceName
+
+        self.URIobjectName = URIRef("http://semantics.id/ns/example#" + self.objectName)
+        self.URIinstanceName = URIRef("http://semantics.id/ns/example/film#fullName")
+
+        self.g.add((self.URIobjectName, RDF.type, OWL.NamedIndividual))
+        self.g.add((self.URIobjectName, self.instanceName, Literal(self.URIinstanceName)))
